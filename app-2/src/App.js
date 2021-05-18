6b0d47322eace728b38b717dc03383537b3912e6
Bret Fernberg <bret.fernberg@gmail.com>
2021-05-18 15:28:23 -0600

    Finished app 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import Spell from './Spell';
 
 function App() {
+  const [spells, setSpells] = useState([])
+
+  useEffect(()=>{
+    axios.get(`https://www.dnd5eapi.co/api/spells?level=5`).then(res=>{
+      setSpells(res.data.results)
+    })
+  }, [])
+  console.log(spells)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {spells.length>0? spells.map(spell=>{
+        return <Spell key={spell.index} spell={spell} />
+      })
+    : 'Loading....'}
     </div>
   );
 }
---
+++ app-2/src/Spell.js
@@ -0,0 +1,4 @@
+const Spell=(props)=>{
+    return <h2>{props.spell.name}</h2>
+}
+export default Spell
