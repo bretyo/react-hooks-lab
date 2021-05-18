871bfd08f4b14f36d7a61d182c9fe076022222b1
Bret Fernberg <bret.fernberg@gmail.com>
2021-05-18 15:11:36 -0600

    changed stuff to follow specifications better

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import AddToDo from './components/AddTodo';
 import './App.css';
 import List from './components/List';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addTodo=(todo)=>{
+    setTodos([...todos, todo])
+  }
+
   return (
     <div className="App">
-      <List/>
+      <AddToDo addTodo={addTodo}/>
+      <List todos={todos}/>
     </div>
   );
 }
--- app-1/src/components/List.js
+++ app-1/src/components/List.js
@@ -1,17 +1,14 @@
-import {useState} from 'react'
-import AddToDo from './AddTodo';
+
 import Todo from './Todo';
 
-const List=()=>{
-    const [todos, setTodos] = useState([]);
+const List=(props)=>{
+    
 
-    const addTodo=(todo)=>{
-        setTodos([...todos, todo])
-    }
+    
     return(
         <div>
-            <AddToDo addTodo={addTodo}/>
-            {todos.length>0 ? todos.map((todo,index)=>{
+            
+            {props.todos.length>0 ? props.todos.map((todo,index)=>{
                 return <Todo key={index} todo={todo}/>
             })
                 : <h3>--Add To-dos Above--</h3>
